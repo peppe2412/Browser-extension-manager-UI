c1965a98af93629792b6de51752215617167798a
peppe2412 <peppeterrazzano79@gmail.com>
2026-02-14 21:31:45 +0100

    Custom data contents

--- main.js
+++ main.js
@@ -14,16 +14,23 @@ fetch("data.json")
 function appendData(data) {
   data.forEach((d) => {
     divElement(`
+      <div class="logo-data-content-box">
         <img src="${d.logo}" alt="${d.name} Logo">
-        <h4>${d.name}</h4>
-        <p>${d.description}</p>
-        <button>${d.isActive}</button>
+      </div>
+      <div>
+        <h4 style="margin: 0; margin-bottom: 5px;" class="bold-font">${d.name}</h4>
+        <small style="margin: 0;" class="regular-font">${d.description}</small>
+        <div class="container-checkbox">
+          <input type="checkbox" title="Attiva">
+        </div>
+      </div>
     `);
   });
 }
 
 function divElement(dataContent) {
   let div = document.createElement("div");
+  div.classList.add("content-data-box");
   div.innerHTML = dataContent;
   content.appendChild(div);
 }
